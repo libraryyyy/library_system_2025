21f884e1fec3362f07cdb239acdb07af0d67636a
s12219454-creator <s12219454@stu.najah.edu>
2025-11-27 13:30:55 +0200

    Changes

---
+++ src/main/java/library_system/service/OverdueReportService.java
@@ -0,0 +1,40 @@
+package library_system.service;
+
+import library_system.Repository.LoanRepository;
+import library_system.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Builds overdue report for a specific user.
+ */
+public class OverdueReportService {
+
+    private FineCalculatorService fineCalculator = new FineCalculatorService();
+
+    public OverdueReport generateReport(User user) {
+
+        List<Loan> userLoans = LoanRepository.getUserLoans(user.getUsername());
+        List<Loan> overdueItems = new ArrayList<>();
+
+        int totalFine = 0;
+        int booksCount = 0;
+        int cdsCount = 0;
+
+        for (Loan loan : userLoans) {
+            if (loan.isOverdue()) {
+                overdueItems.add(loan);
+
+                int fine = fineCalculator.calculateFine(loan);
+                totalFine += fine;
+
+                Media item = loan.getItem();
+                if (item instanceof Book) booksCount++;
+                if (item instanceof CD) cdsCount++;
+            }
+        }
+
+        return new OverdueReport(overdueItems, totalFine, booksCount, cdsCount);
+    }
+}
